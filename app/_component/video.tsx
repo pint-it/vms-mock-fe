1c40b5e2955ea934a415f529d53f58779261f41d
chi.ngo <ngochi3005@gmail.com>
2024-05-28 12:13:43 +0700

    Add poster, remove preload to make video resolution match the 1st iframe

--- app/_component/video.tsx
+++ app/_component/video.tsx
@@ -1,21 +1,28 @@
-'use client';
+"use client";
 
-import { VideoI } from '../_lib/interface/video';
+import { VideoI } from "../_lib/interface/video";
 
-export default function Video({video}: {video: VideoI}) {
+export default function Video({ video }: { video: VideoI }) {
   const hasTitle = video.Subtitles && video.Subtitles.length > 0;
   return (
     <>
       <h2 className="text-2xl">{video.Title}</h2>
       <div>
-        <video controls preload="none" className='py-2 w-full'>
+        <video
+          controls
+          className="py-2 w-full"
+          poster="/video-default-thumbnail-539x303.jpg"
+        >
           <source src={video.CloudFrontUrl} type={video.Type} />
-          {hasTitle && video.Subtitles.map(sub => <track
-            key={sub.url}
-            src={sub.url}
-            kind="subtitles"
-            srcLang={sub.language}
-          /> ) }
+          {hasTitle &&
+            video.Subtitles.map((sub) => (
+              <track
+                key={sub.url}
+                src={sub.url}
+                kind="subtitles"
+                srcLang={sub.language}
+              />
+            ))}
           Your browser does not support the video tag.
         </video>
         <p>{video.MetaData.metadata_view.description}</p>
